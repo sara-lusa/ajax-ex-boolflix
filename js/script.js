b99092cbc9214d7c18c563a952bbc5bf8febef4c
Sara <saracorsob@gmail.com>
2020-07-01 13:04:52 +0200

    Rinchiusa la chiamata in una funzione + aggiunta evento al tasto invio

--- js/script.js
+++ js/script.js
@@ -7,50 +7,63 @@ $(document).ready(function() {
     // Dichiaro in una variabile il valore dell'input
     var valueQuery = $('input').val();
 
-    // Al click, faccio la chiamata ajax all'API,
-    // e inserisco il valore dell'input come query di ricerca
-    $.ajax(
-      {
-        url: 'https://api.themoviedb.org/3/search/movie',
-        method: 'GET',
-        data: {
-          api_key: '9e3b41d15d625e280b597413a544c557',
-          query: valueQuery,
-          language: 'it-IT'
-        },
-        success: function(dataResponse) {
-          var arrayMovies = dataResponse.results;
+    searchAndPrintMovies(valueQuery);
+  });
 
-          // Con handlebars copio il template della scheda film
-          // lo completo con un oggetto contente le info utili
-          // Lo appendo poi al container
-          $('.movie-info').html('');
+  $('input').keypress(function(event) {
+    if(event.which === 13 || event.keyCode === 13) {
+      // Dichiaro in una variabile il valore dell'input
+      var valueQuery = $('input').val();
 
-          var source = $('#movie-template').html();
-          var template = Handlebars.compile(source);
-
-          for (var i = 0; i < arrayMovies.length; i++) {
-            console.log(arrayMovies[i].title);
-
-            var context = {
-              title: arrayMovies[i].title,
-              original_title: arrayMovies[i].original_title,
-              language: arrayMovies[i].original_language,
-              vote: arrayMovies[i].vote_average,
-            };
-            var html = template(context);
-
-            $('.movie-info').append(html);
-          }
-
-        },
-        error: function() {
-          alert('ERROR: ');
-        }
-      }
-    );
+      searchAndPrintMovies(valueQuery);
+    }
   });
 });
 
 // FUNCTIONS
-// Funzione di
+// Funzione di chiamata ajax e handlbars
+function searchAndPrintMovies(valueQuery) {
+  // Al click, faccio la chiamata ajax all'API,
+  // e inserisco il valore dell'input come query di ricerca
+  $.ajax(
+    {
+      url: 'https://api.themoviedb.org/3/search/movie',
+      method: 'GET',
+      data: {
+        api_key: '9e3b41d15d625e280b597413a544c557',
+        query: valueQuery,
+        language: 'it-IT'
+      },
+      success: function(dataResponse) {
+        var arrayMovies = dataResponse.results;
+
+        // Con handlebars copio il template della scheda film
+        // lo completo con un oggetto contente le info utili
+        // Lo appendo poi al container
+        $('.movie-container').html('');
+
+        var source = $('#movie-template').html();
+        var template = Handlebars.compile(source);
+
+        for (var i = 0; i < arrayMovies.length; i++) {
+          console.log(arrayMovies[i].title);
+
+          var context = {
+            title: arrayMovies[i].title,
+            original_title: arrayMovies[i].original_title,
+            language: arrayMovies[i].original_language,
+            vote: arrayMovies[i].vote_average,
+          };
+          var html = template(context);
+
+          $('.movie-container').append(html);
+        }
+
+        $('input').val(''); //------------ TODO: rendere dinamico?
+      },
+      error: function(richiesta, stato, errori) {
+        alert('ERROR:' +  errori);
+      }
+    }
+  );
+}
