ba5dedbdaa266b90477bc8fb0621e08a5cd72971
Sara <saracorsob@gmail.com>
2020-07-01 11:49:27 +0200

    Aggiunta commenti di spiegazione tappe js + creazione struttura html gen

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-alert('ciao');
+$(document).ready(function() {
+  // 9e3b41d15d625e280b597413a544c557
+  // https://api.themoviedb.org/3/movie/550?api_key=9e3b41d15d625e280b597413a544c557
+
+  // Creo evento al click del button
+
+  // Al click, faccio la chiamata ajax all'API,
+  // e inserisco il valore dell'input come query di ricerca
+
+  // Con handlebars copio il template della scheda film
+  // lo completo con un oggetto contente le info utili
+  // Lo appendo poi al container
+});
