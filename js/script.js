0dec61630b183316b7c88176142d4920e3df407d
Sara <saracorsob@gmail.com>
2020-07-01 12:41:43 +0200

    Seconda parte: utilizzo di handlebars con completamento context e append

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
   $('#search').click(function() {
     // Dichiaro in una variabile il valore dell'input
     var valueQuery = $('input').val();
-    console.log(valueQuery);
 
     // Al click, faccio la chiamata ajax all'API,
     // e inserisco il valore dell'input come query di ricerca
@@ -20,11 +19,29 @@ $(document).ready(function() {
           language: 'it-IT'
         },
         success: function(dataResponse) {
-        
+          var arrayMovies = dataResponse.results;
 
           // Con handlebars copio il template della scheda film
           // lo completo con un oggetto contente le info utili
           // Lo appendo poi al container
+          $('.movie-info').html('');
+
+          var source = $('#movie-template').html();
+          var template = Handlebars.compile(source);
+
+          for (var i = 0; i < arrayMovies.length; i++) {
+            console.log(arrayMovies[i].title);
+
+            var context = {
+              title: arrayMovies[i].title,
+              original_title: arrayMovies[i].original_title,
+              language: arrayMovies[i].original_language,
+              vote: arrayMovies[i].vote_average,
+            };
+            var html = template(context);
+
+            $('.movie-info').append(html);
+          }
 
         },
         error: function() {
